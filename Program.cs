34fa6643e0804955b5907c57fdd7841ee780b239
Jeremy Eudaly <eudaly_19@hotmail.comeugit>
2020-03-22 10:59:45 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GHTEST32220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers(10);
+        }
+
+        public static void PrintNumbers (int x)
+        {
+            for(int i = 1; i<= x; i++)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
